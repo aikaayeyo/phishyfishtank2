4834f9d603366c380c7ceef8da850d76fea48ebd
raadiyasabah8-cyber <raadiyasabah8@gmail.com>
2025-10-07 16:40:39 -0700

    fixed calcium code

--- updates/calcium.py
+++ updates/calcium.py
@@ -1,21 +1,20 @@
 def monitor():
-  try:
-
-    calc_levels = [380, 495, 410, 430, 445]
+    # List of calcium levels from 380 to 450 in intervals of 5
+    calc_levels = [380, 385, 390, 395, 400, 405, 410, 415, 420, 425, 430, 435, 440, 445, 450]
 
     current = get_calcium_level()
-    mesg = "Calcium level OK"
-    
-    if (current < calc_levels[0]):
-      mesg = "Calcium level too low!"
-    elif (current > calc_levels[4]):
-      mesg = "Calcium level  too high!"
-    
-  except:
-    print("Unexpected error")
 
-  return mesg
+    if current < 380:
+        mesg = "Calcium level too low!"
+    elif current > 450:
+        mesg = "Calcium level too high!"
+    else:
+        mesg = "Calcium level perfect!"
+    return mesg
   
 # Functiion to simulate actual fish tank monitoring
 def get_calcium_level():
-  return 420
+    return 420
+
+# Print the result when running the script
+print(monitor())
