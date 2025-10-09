8218afb22f84be2534cf691bac23873fc53e4438
aikaayeyo <loveannika18@gmail.com>
2025-10-08 20:44:22 -0700

    fixed magnesium levels

--- updates/magnesium.py
+++ updates/magnesium.py
@@ -1,25 +1,21 @@
 def monitor():
-  try:
 
-    val1 = 0
-    val2 = 0
+    maglvl_low = 1250
+    maglvl_high = 1350
 
-    mag_levels = list(range(val1, val2, 10))
 
     current = get_magnesium_level()
     mesg = "Magnesium level OK"
 
-    num_levels = len(mag_levels)
-    if (current < mag_levels[0]):
-      mesg = "Magnesium level too low!"
-    elif (current > mag_levels[num_levels]):
-      mesg = "Magnesium level too high!"
-    
-  except:
-    print("Unexpected error")
-
-  return mesg
-
+    if (current < maglvl_low):
+      print("Magnesium level too low!")
+    elif (current > maglvl_high):
+      print("Magnesium level too high!")
+    else:
+      print("Magnesium level is normal.")
+      
 # Function to simulate actual fish tank monitoring
 def get_magnesium_level():
   return 1300
+
+monitor()
